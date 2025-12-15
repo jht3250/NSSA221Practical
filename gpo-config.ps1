9e971971de64e416bc49f48765d5c97dbd58b450
John Treon <jht3250@rit.edu>
2025-12-15 13:26:16 -0500

    minor changes

--- gpo-config.ps1
+++ gpo-config.ps1
@@ -7,7 +7,7 @@ Set-GPRegistryValue -Name "DenyControlPanel" `
     -Key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" `
     -ValueName "NoControlPanel" -Type DWord -Value 1
 
-New-GPLink -Name "DenyControlPanel" -Target "DC=abc1234,DC=com"
+New-GPLink -Name "DenyControlPanel" -Target "DC=jht3250,DC=com"
 
 $GPO2 = New-GPO -Name "StandardWallpaper"
 
@@ -15,7 +15,7 @@ Set-GPRegistryValue -Name "StandardWallpaper" `
     -Key "HKCU\Control Panel\Desktop" `
     -ValueName "Wallpaper" -Type String -Value "\\server\share\wallpaper.jpg"
 
-New-GPLink -Name "StandardWallpaper" -Target "OU=Ramones,DC=abc1234,DC=com"
+New-GPLink -Name "StandardWallpaper" -Target "OU=Ramones,DC=jht3250,DC=com"
 
 Get-GPOReport -Name "DenyControlPanel" -ReportType Html -Path "C:\GPOReports\DenyControlPanel.html"
 Get-GPOReport -Name "StandardWallpaper" -ReportType Html -Path "C:\GPOReports\StandardWallpaper.html"
